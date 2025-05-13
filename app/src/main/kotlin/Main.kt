9b86dafdd23200ba997a85bc367ef93089d4a4ec
Subramanya <anantha.subramanya@capgemini.com>
2025-05-13 22:28:39 +0530

    data_types and arithmetic operators

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main(){ // in dart void main() {}
+    val x = 5 // this is a way of creating a var, in dart we use var x = 5;
+
+    /// there is also var in kotlin. The difference between val and var is mentioned below.
+
+    println("Hello World")
+    // println is similar to java. That is printing in new line.
+
+    val y:Int = 6 // in dart, int y = 6;
+
+    /// Other than Int, the other data type obviously available is Float and Double and the difference
+    /// between is storage in memory Float is 32 bits where as Double is 64 bits.
+
+    val z:Float = 6.34f
+    val a:Double = 5.5
+
+    val b:Boolean = true // this is boolean
+    val c:String = "abc" // this is String
+    /// if I want to change the value of c from "abc" to "xyz" then I cannot do it with val, instead,
+    /// I should do it with var. Below is the example of both val and var
+
+    //    c = "xyz" i get an error val cannot be re-assigned.
+    var d:String = "abc"
+    d = "xyz" // this works, but see you cannot change data type of the variable. if its string it has to be string.
+
+    /// lets talk about all of the operators.
+    val operator = Operators()
+    operator.arithmeticOperators()
+
+
+
+
+
+
+}
---
+++ app/src/main/kotlin/Operators.kt
@@ -0,0 +1,27 @@
+public final class Operators {
+
+    fun arithmeticOperators(){
+        val x:Int = 13
+        val y = 6
+        val addition = x + y
+        println("addition: $addition")
+
+        val subtraction  = x - y
+        println("Subtraction: $subtraction")
+
+        val multiplication = x * y
+        println("multiplication: $multiplication")
+
+        val division = x / y // data type doesn't change here. if its Int it is Int.
+        println("Division: $division")
+
+        val modulo = x % y
+        println("Modulo: $modulo")
+
+    }
+
+    fun comparisionOperators(){
+
+    }
+
+}
